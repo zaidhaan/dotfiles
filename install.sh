19d9d1e1cd3579a68193ccc7662e8bb1f26984f1
Zaidhaan Hussain <zaidhaan03@gmail.com>
2023-06-15 06:44:58 +0800

    install: properly install xprofile

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 ln -sf "$XDG_CONFIG_HOME/zsh/.zshenv" $HOME/
-ln -sf "$XDG_CONFIG_HOME/X11/xprofile" $HOME/
+ln -sf "$XDG_CONFIG_HOME/X11/xprofile" $HOME/.xprofile
 ln -sf "$XDG_CONFIG_HOME/.env" $HOME/
