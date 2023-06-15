803a53225f625e6b0db4bd19bfc63ae6928d9910
Zaidhaan Hussain <zaidhaan03@gmail.com>
2023-06-15 21:39:47 +0800

    owo: add confirmation during updates so no need of backups

--- xmonad/scripts/owo-refresh.sh
+++ xmonad/scripts/owo-refresh.sh
@@ -2,18 +2,30 @@
 
 set -euo pipefail
 
-# TODO: move to a verification-based mechanism that way we don't need to hold onto backups
-# in case something goes wrong
-
 PUBLIC_CDN_LISTING=https://whats-th.is/public-cdn-domains.txt
 OWO_DOMAINS="$HOME/.owodomains"
 
-cp "${OWO_DOMAINS}" "${OWO_DOMAINS}.old"
+curl -fsSL $PUBLIC_CDN_LISTING | grep -vE '^#' | grep -v ':' | grep '.' | LC_ALL=C sort > ${OWO_DOMAINS}.new
 
-curl -fsSL $PUBLIC_CDN_LISTING | grep -vE '^#' | grep -v ':' | grep '.' | LC_ALL=C sort > ${OWO_DOMAINS}
-
-changes="$(git diff --shortstat "${OWO_DOMAINS}.old" "${OWO_DOMAINS}" ; exit 0)"
+changes="$(git diff --shortstat "${OWO_DOMAINS}" "${OWO_DOMAINS}.new" ; exit 0)"
 
 changes=${changes:-no changes}
 
-notify-send "Domains updated!" "$changes"
+if [ "$changes" != "no changes" ]; then
+    LINE_CHANGES="$( { git diff -U0 "${OWO_DOMAINS}" "${OWO_DOMAINS}.new"; exit 0; } | grep '^[+-]' | grep -Ev '^(--- a/|\+\+\+ b/)')"
+    cat <<EOF | xmessage -file - -buttons yes:0,no:1
+Changes detected!
+
+Below are the changes:
+$LINE_CHANGES
+
+Do you wish to proceed?
+EOF
+    if [ $? -eq 0 ]; then
+        mv "${OWO_DOMAINS}.new" "${OWO_DOMAINS}"
+        notify-send "Domains updated!" "$changes"
+        exit 0
+    fi
+fi
+
+notify-send "No changes made"
