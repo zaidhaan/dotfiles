a77bafff5a5b89c8e470cae91529206257e99ec9
Zaidhaan Hussain <zaidhaan03@gmail.com>
2023-06-15 21:36:32 +0800

    xresources: update with xmessage and color configuration

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 
 ln -sf "$XDG_CONFIG_HOME/zsh/.zshenv" $HOME/
 ln -sf "$XDG_CONFIG_HOME/X11/xprofile" $HOME/.xprofile
+ln -sf "$XDG_CONFIG_HOME/X11/xresources" $HOME/.Xresources
 ln -sf "$XDG_CONFIG_HOME/.env" $HOME/
